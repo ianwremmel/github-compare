a2725897f787d50a119067ba7c8be8a006a0dc6b
Ian W. Remmel <1182361+ianwremmel@users.noreply.github.com>
2018-10-19 09:36:24 -0700

    feat: switch from bootstrap to GitHub Primer

---
+++ src/components/box.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="Box">
+    <div class="Box-header">
+      <slot 
+        name="header" 
+        :title="title"
+      >
+        <h2 class="Box-title">
+          {{ title }}
+        </h2>
+      </slot>
+    </div>
+    <div class="Box-body">
+      <slot name="body" />
+    </div>
+    <div class="Box-footer">
+      <slot name="footer" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      required: true,
+      type: String
+    }
+  }
+};
+</script>
+
+<style scoped>
+.Box :empty {
+  display: none;
+}
+</style>
